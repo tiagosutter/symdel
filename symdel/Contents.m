d34ce0221905cbda3ba3af53862a58dca470253d
tiagosutter <tiagosutter@gmail.com>
2019-10-31 02:50:13 -0300

    Atualiza Contents.m com copyright e ascii art

--- symdel/Contents.m
+++ symdel/Contents.m
@@ -1,7 +1,20 @@
 % symdel
 % Version 0.1.0 31-Oct-2019
-%
+% 
+%  ___ _   _ _ __ ___   ************
+% /  _| | | | '_ ` _ \   **       *
+% \__ \ |-| | | | | | |   **     *
+% | __/\__, |_| |_| |_|    **   *
+%      |___/                ** *
+%                            **
+%   Arte Ascii gerada com auxilio de:
+%   http://patorjk.com/software/taag/
+%   http://asciiflow.com/
+% 
+%   Conteúdo
+%   --------
 %   CoordSys       - CoordSys Enumeração de sistemas de coordenadas
 %   sym_divergence - Divergente simbolico de um campo vetorial
 %   sym_gradient   - Gradiente simbolico
 
+% Copyright (c) 2019 Tiago Ferreira Sutter
