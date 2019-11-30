a4342cf2a6246982866f21e5ec8ca70a6f9cf299
tiagosutter <tiagosutter@gmail.com>
2019-11-30 15:09:44 -0300

    Deleta post_install.m

--- post_install.m
+++
@@ -1,6 +0,0 @@
-function retval = post_install (desc)
-  disp("Repositório em:")
-  disp(desc.url);
-  disp("Encontre mais documentação em:")
-  disp("https://github.com/tiagosutter/symdel/wiki")
-endfunction
