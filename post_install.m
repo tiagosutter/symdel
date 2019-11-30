b5021e0518fa9c421a0ddf481d82a4af6c6c7703
tiagosutter <tiagosutter@gmail.com>
2019-11-30 13:54:47 -0300

    Cria post_install.m

---
+++ post_install.m
@@ -0,0 +1,6 @@
+function retval = post_install (desc)
+  disp("Repositório em:")
+  disp(desc.url);
+  disp("Encontre mais documentação em:")
+  disp("https://github.com/tiagosutter/symdel/wiki")
+endfunction
